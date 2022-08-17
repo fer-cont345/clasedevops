67da8d68f274af26008b03359bf4eba5f2e1ad79
fer-cont345 <contrerascf0012@gmail.com>
2022-08-17 02:09:33 +0000

    corrected and added Dockerfile

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return{"message":"Hello World"}
+
+@app.get("/myname/{name}")
+async def myName(name: str):
+    return{"message": f"Hello {name} this is my new API"}
