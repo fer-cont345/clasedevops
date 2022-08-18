fe1a6ded21d54011c80a246ba0fbad9edda56b21
fer-cont345 <contrerascf0012@gmail.com>
2022-08-18 02:08:14 +0000

    docker and pipfiles added

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,3 @@ app = FastAPI()
 async def root():
     return{"message":"Hello World"}
 
-@app.get("/myname/{name}")
-async def myName(name: str):
-    return{"message": f"Hello {name} this is my new API"}
