aa2151af1132db0ad29fa52355dd9e5e137eb700
fer-cont345 <contrerascf0012@gmail.com>
2022-08-21 03:36:24 +0000

    Change folder structure

--- docker_fastapi/app/main.py
+++ docker_fastapi/app/main.py
@@ -10,3 +10,7 @@ async def root():
 @app.get("/myname/{name}")
 async def myName(name: str):
     return{"message": f"Hello {name} this is my new API"}
+
+@app.get("/challenge")
+async def writeName():
+    return{"message":"Hey, there!! My name is Fernando Contreras:)"}
